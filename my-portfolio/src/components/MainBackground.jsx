3b79aa3cb80716e5266baf9bdab6473862cd5942
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-05 13:00:00 +0530

    Update MainBackground.jsx

--- my-portfolio/src/components/MainBackground.jsx
+++ my-portfolio/src/components/MainBackground.jsx
@@ -10,6 +10,19 @@ export default function MainBackground() {
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         mountRef.current.appendChild(renderer.domElement);
+        camera.position.z = 5;
+
+        const starGeometry = new THREE.BufferGeometry();
+        const starCount = 10000; //1500
+        const starPositions = new Float32Array(starCount * 3);
+        for (let i = 0; i < starCount*3; i++) {
+            starPositions[i] = (Math.random() - 0.5) * 100;
+        }
+        starGeometry.setAttribute('position', new THREE.BufferAttribute(starPositions, 3));
+        const starMaterial = new THREE.PointsMaterial({color: 0xffffff,size: 0.1});
+        const starField = new THREE.Points(starGeometry, starMaterial);
+        scene.add(starField);
+
     })
   return (
     <div>MainBackground</div>
