4facbfe55f168ba6a035d4806f63eb9031fb924d
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-05 13:20:30 +0530

    Update App.jsx

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,8 +1,10 @@
+import MainBackground from "./components/MainBackground";
+
 function App() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <MainBackground/>
     </>
   );
 }
