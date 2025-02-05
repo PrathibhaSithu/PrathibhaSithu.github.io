6ab3348e9a4edb2d71cbbaed6f9b30c780607d65
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-05 12:52:47 +0530

    Update MainBackground.jsx

--- my-portfolio/src/components/MainBackground.jsx
+++ my-portfolio/src/components/MainBackground.jsx
@@ -1,6 +1,16 @@
 import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
 export default function MainBackground() {
+    const mountRef = useRef(null);
+
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        mountRef.current.appendChild(renderer.domElement);
+    })
   return (
     <div>MainBackground</div>
   );
