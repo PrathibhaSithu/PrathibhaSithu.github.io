86d832f2eb463463b7e6ec33954a13cc39e979d8
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-06 14:14:25 +0530

    Update NavBar.jsx

--- my-portfolio/src/components/NavBar.jsx
+++ my-portfolio/src/components/NavBar.jsx
@@ -14,9 +14,26 @@ export default function NavBar() {
     
     useEffect(() => {
         const menuAnimation = isOpen ? [
+        [
             'nav',
             {transform: "translateX(0%)"},
-            {ease:[0.08, 0.65, 0.53, 0.96]}
+            {ease:[0.08, 0.65, 0.53, 0.96], duration:0.6}
+        ],
+        [
+            'li',
+            {transform: 'scale(1)', opacity:1, filter: 'blur(0px)'},
+            {delay :  stagger(0.05),at: '- 0.1'},
+        ]
+        ]:[
+           [ 'li',
+            {transform: 'scale(0.5)', opacity:0, filter: 'blur(10px)'},
+            {delay :  stagger(0.05, {from: 'last'}), at: '<'},
+           ],
+           [
+            'nav',
+            {transform: "translateX(-100%)"},
+            {at: '-0.1',},
+        ],
         ]
     })
 
