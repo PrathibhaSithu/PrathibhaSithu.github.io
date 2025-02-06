688567761e56c2bc07d2fad95adfd6ae05b2989f
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-06 14:22:58 +0530

    Update NavBar.jsx

--- my-portfolio/src/components/NavBar.jsx
+++ my-portfolio/src/components/NavBar.jsx
@@ -34,8 +34,25 @@ export default function NavBar() {
             {transform: "translateX(-100%)"},
             {at: '-0.1',},
         ],
-        ]
-    })
+        ];
+        animate([
+            [
+                'path.top',
+                {d: isOpen ? 'M 3 16.5 L 17 2.5' : 'M 2 2.5 L 20 2.5'},
+                { at : '<'}
+            ],
+            [
+                'path.middle',
+                {opacity: isOpen ? 0 : 1},{at: '<'}
+            ],
+            [
+                'path.bottom',
+                {d: isOpen ? 'M 3 2.5 L 17 16.346' : 'M 2 16.346 L 20 16.346'},
+                {at: '<'}
+            ],
+            ...menuAnimation
+        ])
+    },[isOpen, animate]);
 
     return(
         <div className="nav-bar">NavBar</div>
