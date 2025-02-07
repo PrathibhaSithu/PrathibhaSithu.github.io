53af522cc55f3792fe790b920916e9a27f89738e
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-07 15:16:30 +0530

    Update config.js

---
+++ my-portfolio/src/components/data/config.js
@@ -0,0 +1,21 @@
+import { easeInOut } from "framer-motion";
+
+const animationDuration = 2;
+export const variants = {
+  initial: { pathLength: 0, strokeOpacity: 1, fillOpacity: 0 },
+  animate: {
+    pathLength: 1,
+    strokeOpacity: 0,
+    fillOpacity: 1,
+    transition: {
+      duration: animationDuration,
+      ease: easeInOut,
+      strokeOpacity: {
+        delay: animationDuration,
+      },
+      fillOpacity: {
+        delay: animationDuration,
+      },
+    },
+  },
+};
