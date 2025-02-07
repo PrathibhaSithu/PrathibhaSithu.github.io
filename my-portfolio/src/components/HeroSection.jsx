c590f5fa534383c2f6ffdf520fcf1f818e6eadcb
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-07 13:20:34 +0530

    Update HeroSection.jsx

---
+++ my-portfolio/src/components/HeroSection.jsx
@@ -0,0 +1,23 @@
+// import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+
+function HeroSection() {
+    return (
+        <section className="relative w-full">
+            <header 
+                className="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#007cda]
+                via-[#785ae4] to-[#A35EA0] opacity-20 blur-[100px] left-10 top-0 hidden md:block">
+            </header>
+            <header 
+                className="absolute w-1/2 aspect-[16/5] -skew-x-12 rounded-full bg-gradient-to-r from-[#007cda]
+                via-[#785ae4] to-[#A35EA0] opacity-20 blur-[100px] right-10 bottom-0 hidden md:block">
+            </header>
+            <section className="w-full px-5 sm:px-8 md:px-12 lg:px-0 max-w-screen-lg lg:max-w-screen-xl mx-auto relative">
+                <article className="grid lg:grid-cols-2 gap-10 xl:gap-6 relative pt-24 lg:max-w-none max-w-2xl md:max-w-3xl mx-auto">
+
+                </article>
+            </section>
+        </section>
+    );
+}
