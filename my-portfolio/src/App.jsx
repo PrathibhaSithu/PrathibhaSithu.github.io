9e6b7cb58820eeeec178958c7da16ab3e5d9e851
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-07 13:20:45 +0530

    Update App.jsx

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -2,15 +2,13 @@
 import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <>
-    <div className="bg-[#f4e1e6]">
-      <NavBar/>
-    </div>
+      <div className="bg-[#f4e1e6]">
+        <NavBar />
+      </div>
       {/* <MainBackground/>
       <NavBar/> */}
-      
     </>
   );
 }
