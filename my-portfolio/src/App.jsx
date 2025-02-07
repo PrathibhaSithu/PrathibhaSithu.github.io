38ed271d96cd2c01fc5ef9c3e5334f2a36d5d007
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-07 10:20:21 +0530

    Update App.jsx

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import MainBackground from "./components/MainBackground";
+import NavBar from "./components/NavBar";
 
 function App() {
 
   return (
     <>
       <MainBackground/>
+      <NavBar/>
     </>
   );
 }
