88aecc8a9788a5b07079310ed6efd7ab7dcb29bc
PrathibhaSithu <sithuminiprathiba@gmail.com>
2025-02-08 09:42:01 +0530

    Update InteractiveButton.jsx

--- my-portfolio/src/components/InteractiveButton.jsx
+++ my-portfolio/src/components/InteractiveButton.jsx
@@ -1,26 +1,58 @@
-import {useRef, useState} from 'react';
-import {motion} from 'framer-motion';
+import { useRef, useState } from "react";
+import { motion } from "framer-motion";
 
-const CYCLE_PER_LETTER =2;
+const CYCLE_PER_LETTER = 2;
 const SHUFFLE_TIME = 50;
-const CHARS = '!@#$%^&*():{};|,<>/?';
+const CHARS = "!@#$%^&*():{};|,<>/?";
 
-export default function InteractiveButton({text}) {
+export default function InteractiveButton({ text }) {
+  return (
+    <div className="flex justify-center items-center min-h-[200px] p-4">
+      <CustomButton text={text} />
+    </div>
+  );
+}
+
+const CustomButton = ({ text }) => {
+  const intervalRef = useRef(null);
+  const [displayText, setDisplayText] = useState(text);
+
+  const scramble = () => {
+    let pos = 0;
+    intervalRef.current = setInterval(() => {
+      const scrambled = text
+        .split("")
+        .map((char, index) => {
+          if (pos / CYCLE_PER_LETTER > index) {
+            return char;
+          }
+          const randomCharIndex = Math.floor(Math.random() * CHARS.length);
+          const randomChar = CHARS[randomCharIndex];
+          return randomChar;
+        })
+        .join("");
+
+      setDisplayText(scrambled);
+      pos++;
+      if (pos >= text.length * CYCLE_PER_LETTER) {
+        stopScramble();
+      }
+    }, SHUFFLE_TIME);
+  };
+  const stopScramble = () => {
+    clearInterval(intervalRef.current || undefined);
+    setDisplayText(text);
+  };
+
     return (
-       <div className='flex justify-center items-center min-h-[200px] p-4'> 
-        <CustomButton text={text}/>
-       </div>
-    )
-}
-
-const CustomButton = ({text}) => {
-    const intervalRef = useRef(null);
-    const [displayText, setDisplayText] = useState(text);
-
-    const scramble = () =>{
-        let pos=0;
-        intervalRef.current = setInterval(()=>{
-           const scrambled = text.split('').map((char, index) =>{})
-    })
-}
-}
+        <motion.button
+        className="w-full px-8 text-lg font-bold text-white rounded-lg overflow-hidden relative bg-gradient-to-r from-blue-500 to-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white"
+        // whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        onMouseEnter={scramble}
+        onMouseLeave={stopScramble}
+        >
+        {displayText}
+        </motion.button>
+    );
+};
