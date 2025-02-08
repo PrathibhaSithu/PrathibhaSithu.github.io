746cd604b50d6ceff5e1032d444c531a67754be6
PrathibhaSithu <sithuminiprathiba@gmail.com>
2025-02-08 23:15:24 +0530

    Update InteractiveButton.jsx

--- my-portfolio/src/components/InteractiveButton.jsx
+++ my-portfolio/src/components/InteractiveButton.jsx
@@ -10,7 +10,7 @@ export default function InteractiveButton({ text }) {
     <div className="flex justify-center items-center min-h-[200px] p-4">
       <CustomButton text={text} />
     </div>
-  );
+  )
 }
 
 const CustomButton = ({ text }) => {
@@ -44,18 +44,20 @@ const CustomButton = ({ text }) => {
     setDisplayText(text);
   };
 
-    return (
-        <motion.button
-        className="w-full px-8 text-lg font-bold text-white rounded-lg overflow-hidden relative 
-        bg-gradient-to-r from-blue-500 to-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white"
-        // whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.95 }}
-        onMouseEnter={scramble}
-        onMouseLeave={stopScramble}
-        >
-          <span className="absolute inset-0 w-full h-full bg-[#ff5c72] rounded-lg transform scale-0
-          transition-transform duration-300 ease-out group-hover:scale-100"></span>
-        <span className="relative z-10">{displayText}</span>
-        </motion.button>
-    );
+  return (
+    <motion.button
+      whileTap={{ scale: 0.95 }}
+      onMouseEnter={scramble}
+      onMouseLeave={stopScramble}
+      className="w-full px-8 text-lg font-bold text-white rounded-lg overflow-hidden relative 
+        bg-gradient-to-r from-pink-900 to-pink-700"
+      // whileHover={{ scale: 1.1 }}
+    >
+      <span
+        className="absolute inset-0 w-full h-full bg-[#ff5c72] rounded-lg transform scale-0
+          transition-transform duration-300 ease-out group-hover:scale-100"
+      ></span>
+      <span className="relative z-10">{displayText}</span>
+    </motion.button>
+  );
 };
