ae4b2c16e3ebda982119ac48d33b54b2773c73b3
PrathibhaSithu <sithuminiprathiba@gmail.com>
2025-02-09 13:37:49 +0530

    Update SocialButton.jsx

--- my-portfolio/src/components/SocialButton.jsx
+++ my-portfolio/src/components/SocialButton.jsx
@@ -25,16 +25,31 @@ export default function SocialButton() {
         transition: {
           duration: 0.3,
         },
-    }
-    }
-};
+      },
+    },
+  };
 
   return (
-    <div className="md:flex flex-col items-center justify-center border border-[#720c61] bg-[#ffffff29] 
-    rounded-3xl space-y-11 p-3 max-h-[506px] md:max-h-[386px] hidden">
+    <div
+      className="md:flex flex-col items-center justify-center border border-[#720c61] bg-[#ffffff29] 
+    rounded-3xl space-y-11 p-3 max-h-[506px] md:max-h-[386px] hidden"
+    >
       {socialIcons.map((icon) => (
-        <button key={icon.id}></button>
+        <button key={icon.id}>
+          <svg viewBox={icon.viewBox} width={40} height={40}>
+            <motion.path
+              d={icon.path}
+              fill="#ffc107"
+              stroke="#ffc107"
+              strokeWidth={1}
+              variants={variants}
+              initial="initial"
+              animate="animate"
+              whileHover="hover"
+            />
+          </svg>
+        </button>
       ))}
     </div>
-)
+  );
 }
