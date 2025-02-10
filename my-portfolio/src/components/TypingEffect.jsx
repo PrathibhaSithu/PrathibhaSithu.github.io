e3a7c4c56aad49ff5aaee17fd9a4092624ccc675
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2025-02-10 11:47:37 +0530

    Create TypingEffect.jsx

---
+++ my-portfolio/src/components/TypingEffect.jsx
@@ -0,0 +1,20 @@
+import Typed from "react-typed";
+
+export default function TypingEffect() {
+  return (
+    <h2 className="mt-2 text-2xl text-[#720c61] font-semibold">
+      <Typed
+        strings={[
+          "UI/UX Designer",
+          "Web Developer",
+          "Frontend Developer",
+          "Android Developer",
+          "Project Manager",
+        ]}
+        typeSpeed={80} 
+        backSpeed={50} 
+        loop           
+      />
+    </h2>
+  );
+}
